39ce8dffa270ab81a55b9d06c6f1df9cf304d95c
Beatrix <beatrix@sourcegraph.com>
2022-11-16 18:47:33 -0800

    Release beta version: v0.0.0

--- sgup.rb
+++ sgup.rb
@@ -4,8 +4,8 @@
 class Sgup < Formula
   desc "Launch a local Sourcegraph instance from your command line tool via homebrew."
   homepage "https://github.com/sourcegraph/UP"
-  url "https://github.com/sourcegraph/UP/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "e92a866726c15f0dbf71d9f808ec33cbad4a6913c806771d2be364388b0f8e16"
+  url "https://github.com/sourcegraph/UP/archive/refs/tags/v0.0.0.tar.gz"
+  sha256 "d7fe4660fc72a2bfaee9c97d971e20a16de3201d8683c6bfd0dff6640d5dbd20"
   license ""
 
   # depends_on "cmake" => :build
