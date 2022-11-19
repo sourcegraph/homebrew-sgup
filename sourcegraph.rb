6d2eeda6a64850c80201b8b6020154a6c0ef117d
Beatrix <beatrix@sourcegraph.com>
2022-11-18 17:57:55 -0800

    Release v0.0.2

--- sourcegraph.rb
+++ sourcegraph.rb
@@ -4,8 +4,8 @@
 class Sourcegraph < Formula
   desc "Launch a local Sourcegraph instance from your command line tool via homebrew."
   homepage "https://github.com/sourcegraph/homebrew-sourcegraph"
-  url "https://github.com/sourcegraph/homebrew-sourcegraph/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "117d48bfb35277bdc27f7d46591cfea729f2dc5080289653a9883a27a0210b48"
+  url "https://github.com/sourcegraph/homebrew-sourcegraph/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "2e134ec183fce2437ecaf0e3276040fc497b1ae58d1913092f0bbd29a67e38ca"
   license ""
 
   # depends_on "cmake" => :build
